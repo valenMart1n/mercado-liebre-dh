ab06932bccd4320bd5883c8dfbeb86d364f48d0b
Valen Martin <valenmart373@gmail.com>
2023-01-04 21:12:10 -0300

    Agrego proyecto de mercado liebre

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Servidor funcionando en el puerto " + port);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
